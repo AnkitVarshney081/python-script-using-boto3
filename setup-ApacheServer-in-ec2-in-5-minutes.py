fa3a5ffc6eeadd662e4b07792d6701f3e361a16d
AnkitVarshney081 <47515726+AnkitVarshney081@users.noreply.github.com>
2020-05-17 16:09:52 +0530

    Update setup-ApacheServer-in-ec2-in-5-minutes.py

--- setup-ApacheServer-in-ec2-in-5-minutes.py
+++ setup-ApacheServer-in-ec2-in-5-minutes.py
@@ -1,31 +1,44 @@
+# 
+
 import boto3
 import pprint
 session=boto3.Session(profile_name='default')
 vpc=session.resource(service_name='ec2', region_name='ap-south-1')
 
-vpc_cidr = raw_input("Enter VPC Cidr: ")
-subnet_cidr = raw_input("Enter Public Subnet Cidr: ")
+# Create VPC
+
+vpc_cidr = raw_input("Enter VPC Cidr: ")                       # Example - Cidr = 10.200.0.0/16
+subnet_cidr = raw_input("Enter Public Subnet Cidr: ") 	       # Example - Cidr = 10.200.0.0/24
 
 vpc_id = vpc.create_vpc(
-    CidrBlock=vpc_cidr,
-    AmazonProvidedIpv6CidrBlock=False,
-    InstanceTenancy='default'
+    CidrBlock=vpc_cidr,					      # IPv4 CIDR block with the VPC
+    AmazonProvidedIpv6CidrBlock=False,                        # I do not associate an IPv6 CIDR block with the VPC
+    InstanceTenancy='default'			              # You can run instances in your VPC on single-tenant, dedicated hardware. 
+							      #	Select Dedicated to ensure that instances launched in this VPC are dedicated tenancy instances, 
+	                                                      # regardless of the tenancy attribute specified at launch. 
+	                                                      # Select Default to ensure that instances launched in this VPC use the tenancy attribute specified at launch
 )
 
-print vpc_id.id
+print vpc_id.id   # Example vpc.id = vpc-031b9119cd3ea4078
+
+# Create Public Subnet
 
 response = vpc.create_subnet(
-    AvailabilityZone='ap-south-1a',
-    CidrBlock=subnet_cidr,
-    VpcId=vpc_id.id
+    AvailabilityZone='ap-south-1a',                           # you can choose any Availablity Zone in selected Region
+    CidrBlock=subnet_cidr,				      # IPv4 CIDR block with the Subnet = 10.200.0.0/24
+    VpcId=vpc_id.id                                           # Associate subnet with vpc
 )
-print response.id
+print response.id						# Subnet Id
+
+# Create Route Table
 
 route_table = vpc.create_route_table(
-    VpcId=vpc_id.id
+    VpcId=vpc_id.id                                           # Associate Route Table with VPC
 )
 
-print route_table.id
+print route_table.id						# Id of Route Table
+
+# Create Internet Gateway
 
 internet_gateway = vpc.create_internet_gateway()
 
@@ -33,12 +46,14 @@ print internet_gateway.id
 
 vpc_cli=session.client(service_name='ec2', region_name='ap-south-1')
 
-attach = vpc_cli.attach_internet_gateway(
+# Attach Internet Gateway with VPC
+
+vpc_cli.attach_internet_gateway(
     InternetGatewayId=internet_gateway.id,
     VpcId=vpc_id.id
 )
 
-print "Attach IGW to VPC"
+# Subnet Associations - Route Table
 
 ass_rt = vpc_cli.associate_route_table(
     RouteTableId= route_table.id,
